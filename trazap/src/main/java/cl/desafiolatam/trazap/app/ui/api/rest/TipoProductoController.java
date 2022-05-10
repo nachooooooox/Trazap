d7e9b2068c23b642bcf83919b928ec6fe61f34bb
Carlos Chandía <cgchrnani@gmail.com>
2022-05-10 00:07:18 -0400

    Trazap finalizado

--- trazap/src/main/java/cl/desafiolatam/trazap/app/ui/api/rest/TipoProductoController.java
+++ trazap/src/main/java/cl/desafiolatam/trazap/app/ui/api/rest/TipoProductoController.java
@@ -18,7 +18,7 @@ import cl.desafiolatam.trazap.app.repository.dao.model.TipoProducto;
 import cl.desafiolatam.trazap.app.service.response.ResponseServiceObject;
 
 @RestController
-@RequestMapping("/tipoProducto")
+@RequestMapping("/admin/tipoProductoApi")
 public class TipoProductoController {
 
 	@Autowired
